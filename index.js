01bde267d74ababa6fc63abd02fa80deff6d37dc
Abdoul Djelil Sana <sanaabdouldjelil5@gmail.com>
2025-03-04 08:14:43 +0000

    ajout de html, javascript et css

---
+++ index.js
@@ -0,0 +1,40 @@
+
+//img food
+let lienfood=["img1","img2","img3","img4","img5","img6","img7","img8","img9","img10"];
+let sizefood=lienfood.length
+let indexfood=Math.floor(Math.random()*sizefood)
+let imgfood=lienfood[indexfood]
+document.getElementById('tab1').innerHTML='<a href="food.html"><img class="clic" alt="image de nouritures" title="Cliquez ici" src="img/foods/'+imgfood+'.jpg"><figcaption><strong>FOODS UPSP</strong></figcaption></a>';
+//end img food
+
+//img cloths
+let liencloths=["img0","img1","img2","img3","img4","img5","img6","img7","img8","img9","img10","img11","img12","img13","img14","img15","img16","img17","img18","img19","img20","img21","img22","img23"];
+let sizecloths=liencloths.length
+let indexcloths=Math.floor(Math.random()*sizecloths)
+let imgcloths=liencloths[indexcloths]
+document.getElementById('tab2').innerHTML='<a href="cloth.html"><img class="clic" alt="image de vêtements" title="Cliquez ici" src="img/cloths/'+imgcloths+'.jpg"><figcaption><strong>CLOTHES & SHOES</strong></figcaption></a>';
+//end img cloths
+
+//img cosmetique
+let liencosmetique=["img0","img1","img2","img3","img4","img5","img6","img7","img8","img9","img10","img11","img12","img13","img14","img15","img16"];
+let sizecosmetique=liencosmetique.length
+let indexcosmetique=Math.floor(Math.random()*sizecosmetique)
+let imgcosmetique=liencosmetique[indexcosmetique]
+document.getElementById('tab3').innerHTML='<a href="cos.html"><img class="clic" alt="image de produits cosmetiques" title="Cliquez ici" src="img/cosmétique/'+imgcosmetique+'.jpg"><figcaption><strong>COSMETIQUES</strong></figcaption></a>';
+//end img cosmetique
+
+//img Tech
+let lienTech=["img0","img1","img2","img3","img4","img5","img6","img7","img8"];
+let sizeTech=lienTech.length
+let indexTech=Math.floor(Math.random()*sizeTech)
+let imgTech=lienTech[indexTech]
+document.getElementById('tab4').innerHTML='<a href="tech.html"><img class="clic" alt="image de produits de technologies" title="Cliquez ici" src="img/Tech/'+imgTech+'.jpg"><figcaption><strong>UPSP TECH</strong></figcaption></a>';
+//end img tech
+
+//img services
+let lienService=["img0","img1","img2","img3","img4","img5","img6","img7","img8"];
+let sizeService=lienService.length
+let indexService=Math.floor(Math.random()*sizeService)
+let imgService=lienService[indexService]
+document.getElementById('tab5').innerHTML='<a href="serv.html"><img class="clic" alt="image exemple de service" title="Cliquez ici" src="img/service/'+imgService+'.jpg"><figcaption><strong>UPSP SERVICES</strong></figcaption></a>';
+//end img services
